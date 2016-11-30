a679d43be9e9dec0b8d1ce828dcad00635f7e0b8
Vahaagn <matgizq@gmail.com>
2016-11-30 22:32:24 +0100

    Segregated Buses into its own folders, Added new Query and its handler

---
+++ src/NetAdmin.Infrastructure/Queries/SqlQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetAdmin.Infrastructure
+{
+    public class SqlQuery : IQuery
+    {
+        public string Hostname { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Query { get; set; }
+    }
+}
